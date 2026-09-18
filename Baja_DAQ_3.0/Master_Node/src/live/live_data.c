45401a99a5b734916628c88dc6ae9c084722bda4
BajaRemy <remy.laurendeau@mail.mcgill.ca>
2026-09-18 13:58:15 -0400

    Add master GPS telemetry and GPS-only profile

--- Baja_DAQ_3.0/Master_Node/src/live/live_data.c
+++ Baja_DAQ_3.0/Master_Node/src/live/live_data.c
@@ -16,6 +16,9 @@ static const live_signal_metadata_t signals[LIVE_DATA_SIGNAL_COUNT] = {
     {0x0BB, "engine_rpm", "engine_node_5", "rpm", 100},
     {0x0BC, "engine_spark", "engine_node_5", "event", 100},
     {0x0BD, "engine_wheel_rpm", "master_pair_node_4_5", "rpm", 100},
+    {0x700, "gps_speed", "master_gps", "km/h_x100", 1},
+    {0x701, "gps_latitude", "master_gps", "deg_e7", 1},
+    {0x702, "gps_longitude", "master_gps", "deg_e7", 1},
 };
 
 static live_sample_t latest[LIVE_DATA_SIGNAL_COUNT];
--- Baja_DAQ_3.0/Master_Node/src/main.c
+++ Baja_DAQ_3.0/Master_Node/src/main.c
@@ -7,6 +7,7 @@
 #include "protocol/rpm_pairing.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "gps/gps_receiver.h"
 #include "logger/data_logger.h"
 #include "live/live_data.h"
 #include "node_state/node_registry.h"
@@ -34,6 +35,7 @@ static void print_status(void)
     app_control_print_status();
 }
 
+#if MASTER_CAN_ENABLED
 static void handle_can_message(const can_message_t *message)
 {
     static rpm_pairing_t pairing;
@@ -56,6 +58,13 @@ static void handle_can_message(const can_message_t *message)
         }
     }
 }
+#endif
+
+static void handle_gps_sample(const can_message_t *sample)
+{
+    data_logger_enqueue(sample);
+    live_data_record(sample);
+}
 
 static void auto_start_task(void *argument)
 {
@@ -71,14 +80,19 @@ void app_main(void)
     ESP_ERROR_CHECK(data_logger_init());
     ESP_ERROR_CHECK(live_data_init());
     ESP_ERROR_CHECK(node_registry_init());
+#if MASTER_CAN_ENABLED
     ESP_ERROR_CHECK(can_master_init(handle_can_message));
+#endif
+    ESP_ERROR_CHECK(gps_receiver_start(handle_gps_sample));
     ESP_ERROR_CHECK(app_control_init());
 
+#if MASTER_CAN_ENABLED
     /* Force nodes idle before the console and automatic session can start. */
     for (int i = 0; i < 3; i++) {
         ESP_ERROR_CHECK_WITHOUT_ABORT(can_master_stop_nodes());
         vTaskDelay(pdMS_TO_TICKS(50));
     }
+#endif
 
     serial_console_callbacks_t console = {
         .start = start_logging,
@@ -86,7 +100,11 @@ void app_main(void)
         .status = print_status,
     };
     ESP_ERROR_CHECK(serial_console_start(&console));
+#if MASTER_CAN_ENABLED
     ESP_ERROR_CHECK(time_beacon_start());
+#else
+    ESP_LOGW(TAG, "CAN disabled; running as a GPS-only master");
+#endif
     ESP_ERROR_CHECK(xTaskCreate(auto_start_task, "auto_start", 3072, NULL, 5, NULL)
                     == pdPASS ? ESP_OK : ESP_ERR_NO_MEM);
     esp_err_t web_error = web_server_start();
