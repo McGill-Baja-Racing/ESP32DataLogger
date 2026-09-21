14a7842460e88ae5c5fe2a4ac74b1c7d57018efc
BajaRemy <remy.laurendeau@mail.mcgill.ca>
2026-09-21 16:52:59 -0400

    Preserve separate RPM sensors and reviewed mainline configuration

--- Baja_DAQ_3.0/Master_Node/src/protocol/app_protocol.h
+++ Baja_DAQ_3.0/Master_Node/src/protocol/app_protocol.h
@@ -15,7 +15,6 @@
 #define CAN_ID_GENERIC_ADC          0x0BA
 #define CAN_ID_ENGINE_RPM           0x0BB
 #define CAN_ID_ENGINE_SPARK         0x0BC
-#define CAN_ID_ENGINE_WHEEL_RPM     0x0BD
 #define CAN_ID_GPS_SPEED            0x700
 #define CAN_ID_GPS_LATITUDE         0x701
 #define CAN_ID_GPS_LONGITUDE        0x702
@@ -35,8 +34,7 @@ static inline bool protocol_is_sensor_id(uint32_t id)
            id == CAN_ID_BEARING_ENCODER ||
            id == CAN_ID_GENERIC_ADC ||
            id == CAN_ID_ENGINE_RPM ||
-           id == CAN_ID_ENGINE_SPARK ||
-           id == CAN_ID_ENGINE_WHEEL_RPM;
+           id == CAN_ID_ENGINE_SPARK;
 }
 
 static inline uint8_t protocol_sensor_node_id(uint32_t id)
@@ -46,7 +44,6 @@ static inline uint8_t protocol_sensor_node_id(uint32_t id)
         case CAN_ID_REAR_BRAKE:     return 1;
         case CAN_ID_BEARING_ENCODER:return 4;
         case CAN_ID_ENGINE_SPARK:
-        case CAN_ID_ENGINE_WHEEL_RPM:
         case CAN_ID_ENGINE_RPM:     return 5;
         case CAN_ID_GENERIC_ADC:    return 6;
         default:                    return 0;
--- Baja_DAQ_3.0/Master_Node/tools/decode_log.py
+++ Baja_DAQ_3.0/Master_Node/tools/decode_log.py
@@ -30,7 +30,6 @@ SIGNAL_METADATA = {
     0x0BA: ("generic_adc_voltage", "adc_node_6", "mV"),
     0x0BB: ("engine_rpm", "engine_node_5", "rpm"),
     0x0BC: ("engine_spark", "engine_node_5", "event"),
-    0x0BD: ("engine_wheel_rpm", "master_pair_node_4_5", "rpm"),
     0x700: ("gps_speed", "master_gps", "km/h_x100"),
     0x701: ("gps_latitude", "master_gps", "deg_e7"),
     0x702: ("gps_longitude", "master_gps", "deg_e7"),
